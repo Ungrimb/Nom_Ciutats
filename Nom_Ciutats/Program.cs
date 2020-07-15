e5f08c9982aa5e88d277a5293812ae38f2666461
Ismael <arkarash@gmail.com>
2020-07-15 09:20:24 +0200

    FASE1

---
+++ Nom_Ciutats/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Nom_Ciutats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ciutat1, ciutat2, ciutat3, ciutat4, ciutat5, ciutat6;
+            Console.WriteLine("Nom ciutat 1 :");
+            ciutat1 = Console.ReadLine();
+            Console.WriteLine("Nom ciutat 2 :");
+            ciutat2 = Console.ReadLine();
+            Console.WriteLine("Nom ciutat 3 :");
+            ciutat3 = Console.ReadLine();
+            Console.WriteLine("Nom ciutat 4 :");
+            ciutat4 = Console.ReadLine();
+            Console.WriteLine("Nom ciutat 5 :");
+            ciutat5 = Console.ReadLine();
+            Console.WriteLine("Nom ciutat 6 :");
+            ciutat6 = Console.ReadLine();
+            Console.WriteLine("Las ciudades introducidas son: ");
+            Console.WriteLine(ciutat1);
+            Console.WriteLine(ciutat2);
+            Console.WriteLine(ciutat3);
+            Console.WriteLine(ciutat4);
+            Console.WriteLine(ciutat5);
+            Console.WriteLine(ciutat6);
+        }
+    }
+}
