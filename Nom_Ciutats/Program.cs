67ebad9e11c0b363edfea604b0b6ccf4ee121f82
Ismael <arkarash@gmail.com>
2020-07-15 12:12:42 +0200

    Girar Nombres

--- Nom_Ciutats/Program.cs
+++ Nom_Ciutats/Program.cs
@@ -19,13 +19,30 @@ namespace Nom_Ciutats
             ciutat5 = Console.ReadLine();
             Console.WriteLine("Nom ciutat 6 :");
             ciutat6 = Console.ReadLine();
-            Console.WriteLine("Las ciudades introducidas son: ");
-            Console.WriteLine(ciutat1);
-            Console.WriteLine(ciutat2);
-            Console.WriteLine(ciutat3);
-            Console.WriteLine(ciutat4);
-            Console.WriteLine(ciutat5);
-            Console.WriteLine(ciutat6);
+
+            string[] arrayCiutats = { ciutat1, ciutat2, ciutat3, ciutat4, ciutat5, ciutat6 };
+
+            char[] chars1 = new char[ciutat1.Length];
+            chars1 = ciutat1.ToCharArray();
+            char[] chars2 = new char[ciutat2.Length];
+            chars2 = ciutat2.ToCharArray();
+            char[] chars3 = new char[ciutat3.Length];
+            chars3 = ciutat3.ToCharArray();
+            char[] chars4 = new char[ciutat4.Length];
+            chars4 = ciutat4.ToCharArray(); 
+            char[] chars5 = new char[ciutat5.Length];
+            chars5 = ciutat5.ToCharArray();
+            char[] chars6 = new char[ciutat6.Length];
+            chars6 = ciutat6.ToCharArray();
+
+            char[][] chars = { chars1, chars2, chars3, chars4, chars5, chars6 };
+
+            foreach (var ciutat in chars)
+            {
+                Console.Write(string.Join("", ciutat)+" - ");
+                Array.Reverse(ciutat);
+                Console.WriteLine(ciutat);
+            }
         }
     }
 }
